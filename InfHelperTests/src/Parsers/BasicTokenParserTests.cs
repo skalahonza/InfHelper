e7e6a92fa19d0ebca589f880c35074c15d34eac9
Jan Skála <j.skala@hotmail.com>
2017-08-13 00:07:19 +0200

    Add ignored tokens test.

--- InfHelperTests/src/Parsers/BasicTokenParserTests.cs
+++ InfHelperTests/src/Parsers/BasicTokenParserTests.cs
@@ -1,19 +1,15 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using InfHelper.Parsers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using InfHelper.Models.Tokens;
+using InfHelper.Parsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace InfHelper.Parsers.Tests
+namespace InfHelperTests.Parsers
 {
     [TestClass()]
     public class BasicTokenParserTests
     {
         [TestMethod()]
-        public void BasicTokenParserTest()
+        public void TokenOrderTest()
         {
             string formula = "[TEST]";
             var parser = new BasicTokenParser(BasicTokenParser.AllAvailableTokens, new HashSet<IToken>()
@@ -27,6 +23,29 @@ namespace InfHelper.Parsers.Tests
             parser.ParseToken(formula);
 
             Assert.AreEqual(formula, result);
+        }
+
+        [TestMethod()]
+        public void IgnoredTokensTest()
+        {
+            string expression = "[TEST]";
+            string formula = "  \n   "+ expression +"   \n   ";
+            var parser = new BasicTokenParser(BasicTokenParser.AllAvailableTokens, new HashSet<IToken>()
+            {
+                new CategoryOpeningToken(),
+                new LetterToken(),
+                new CategoryClosingToken(),
+            }, new HashSet<IToken>()
+            {
+                new WhiteSpaceToken(),
+                new NewLineToken()
+            });
+
+            string result = "";
+            parser.ValidTokenFound += (sender, token) => result += token.Symbol;
+            parser.ParseToken(formula);
+
+            Assert.AreEqual(expression, result);
         }
     }
 }
