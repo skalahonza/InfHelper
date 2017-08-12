bdbcc9a23bbeeb0288fea341243e1b75fa783b1e
Jan Skála <j.skala@hotmail.com>
2017-08-12 23:50:42 +0200

    Fix events, add key id parsing rules.

--- InfHelper/src/Parsers/BasicTokenParser.cs
+++ InfHelper/src/Parsers/BasicTokenParser.cs
@@ -2,16 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using InfHelper.Exceptions;
-using InfHelper.Models;
 using InfHelper.Models.Tokens;
 
 namespace InfHelper.Parsers
 {
     public class BasicTokenParser : ITokenParser
     {
-        private EventHandler<IToken> validTokenFound;
-        private EventHandler<IToken> invalidTokenFound;
-
         public ICollection<IToken> AllTokens { get; set; }
         public ICollection<IToken> AllowedTokens { get; set; }
         public ICollection<IToken> IgnoredTokens { get; set; }
@@ -27,7 +23,7 @@ namespace InfHelper.Parsers
                 new CategoryOpeningToken(),
                 new EqualityToken(),
                 new InlineCommentToken(),
-                new NewLineToken(),                
+                new NewLineToken(),
                 new WhiteSpaceToken(),
                 new LineConcatenatorToken(),
                 new LetterToken(),
@@ -39,20 +35,20 @@ namespace InfHelper.Parsers
             AllowedTokens = allowedTokens;
         }
 
-        public BasicTokenParser(ICollection<IToken> allTokens, ICollection<IToken> allowedTokens):this(allowedTokens)
+        public BasicTokenParser(ICollection<IToken> allTokens, ICollection<IToken> allowedTokens) : this(allowedTokens)
         {
             AllTokens = allTokens;
         }
 
         public virtual void ParseToken(string formula)
-        {            
+        {
             foreach (var c in formula)
             {
                 bool found = false;
 
                 //examine all known tokens
                 foreach (var token in AllTokens)
-                {                    
+                {
                     if (!token.Symbols.Contains(c)) continue;
 
                     //token found
@@ -62,16 +58,16 @@ namespace InfHelper.Parsers
                     //not allowed token detected
                     if (AllowedTokens.All(x => x.Type != token.Type))
                     {
-                        invalidTokenFound?.Invoke(this, token);
-                        continue;                        
+                        InvalidTokenFound?.Invoke(this, token);
+                        continue;
                     }
 
                     //ignored token detected
-                    if(IgnoredTokens.Any(x => x.Type == token.Type))
-                        continue;                    
+                    if (IgnoredTokens.Any(x => x.Type == token.Type))
+                        continue;
 
                     //allowed token detected
-                    validTokenFound?.Invoke(this,token);
+                    ValidTokenFound?.Invoke(this, token);
                 }
 
                 //token not recognized
@@ -80,150 +76,4 @@ namespace InfHelper.Parsers
             }
         }
     }
-
-    public class ContentParser
-    {
-        private Category currentCategory;
-        private Key currentKey;
-        private readonly ITokenParser parser;
-
-        public ContentParser():this(new BasicTokenParser())
-        {
-        }
-
-        public ContentParser(ITokenParser parser)
-        {
-            this.parser = parser;
-            parser.InvalidTokenFound += InvalidTokenFound;
-            InitMainParsing();
-        }
-
-        /// <summary>
-        /// When category parsing is completed
-        /// </summary>
-        public EventHandler<Category> CategoryDiscovered { get; set; }
-
-        private void InitMainParsing()
-        {
-            parser.ValidTokenFound -= ValidTokenFoundDuringKeyParsing;
-            parser.ValidTokenFound += ValidTokenFoundDuringMainParsing;
-
-            parser.AllowedTokens = new HashSet<IToken>
-            {
-                new InlineCommentToken(),
-                new CategoryOpeningToken(),
-            };
-
-            parser.IgnoredTokens = new HashSet<IToken>
-            {
-                new WhiteSpaceToken(),
-                new NewLineToken()
-            };
-        }
-
-        private void InitCategoryParsing()
-        {
-            parser.ValidTokenFound -= ValidTokenFoundDuringMainParsing;
-            parser.ValidTokenFound += ValidTokenFoundDuringCategoryParsing;
-            parser.IgnoredTokens = new HashSet<IToken>();
-            parser.AllowedTokens = new HashSet<IToken>
-            {
-                new CategoryClosingToken(),
-                new LetterToken()
-            };
-        }
-
-        private void InitKeyValuesParsing()
-        {
-            parser.ValidTokenFound -= ValidTokenFoundDuringCategoryParsing;
-            parser.ValidTokenFound += ValidTokenFoundDuringKeyParsing;
-
-            parser.AllowedTokens = new HashSet<IToken>
-            {                
-                new CategoryOpeningToken(),
-                new InlineCommentToken(),
-                new LetterToken(),
-                new EqualityToken(),
-                new LineConcatenatorToken(),
-                new NewLineToken()
-            };
-
-            parser.IgnoredTokens = new HashSet<IToken>
-            {
-                new WhiteSpaceToken()
-            };
-        }
-
-        //Parsing top layer
-        private void ValidTokenFoundDuringMainParsing(object sender, IToken token)
-        {
-            switch (token.Type)
-            {
-                case TokenType.InlineComment:
-                    // TODO go to next line
-                    break;
-                case TokenType.CategoryOpening:
-                    currentCategory = new Category();
-                    InitCategoryParsing();
-                    break;
-                default:
-                    throw new InvalidTokenException("Invalid token found during parsing of the file: " + token.Symbol);
-            }
-        }
-
-        //when parsing a category
-        private void ValidTokenFoundDuringCategoryParsing(object sender, IToken token)
-        {
-            switch (token.Type)
-            {
-                case TokenType.CategoryClosing:
-                    InitKeyValuesParsing();
-                    break;                
-                case TokenType.Letter:
-                    currentCategory.Name += token.Symbol;
-                    break;                
-                default:
-                    throw new InvalidTokenException("Invalid token found during parsing of the file: " + token.Symbol);
-            }
-        }
-
-        private void ValidTokenFoundDuringKeyParsing(object sender, IToken token)
-        {
-            switch (token.Type)
-            {
-                case TokenType.InlineComment:
-                    break;
-                case TokenType.EQ:
-                    break;
-                case TokenType.WhiteSpace:
-                    break;
-                case TokenType.Letter:
-                    break;
-                case TokenType.NewLine:
-                    break;
-                case TokenType.LineConcatenator:
-                    break;
-                default:
-                    throw new InvalidTokenException("Invalid token found during parsing of the file: " + token.Symbol);
-            }
-        }
-
-        private void InvalidTokenFound(object sender, IToken token)
-        {
-            throw new InvalidTokenException("Invalid token found during parsing of the file: " + token.Symbol);
-        }        
-
-        private void KeyParsingComplete()
-        {
-            currentCategory.Keys.Add(currentKey);
-            currentKey = null;
-        }
-
-        private void CategoryParsingComplete()
-        {
-            CategoryDiscovered?.Invoke(this, currentCategory);
-            currentCategory = null;
-        }
-    }
-
 }
---
+++ InfHelper/src/Parsers/ContentParser.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using InfHelper.Exceptions;
+using InfHelper.Models;
+using InfHelper.Models.Tokens;
+
+namespace InfHelper.Parsers
+{
+    public class ContentParser
+    {
+        private Category currentCategory;
+        private Key currentKey;
+        private readonly ITokenParser parser;
+
+        public ContentParser() : this(new BasicTokenParser())
+        {
+        }
+
+        public ContentParser(ITokenParser parser)
+        {
+            this.parser = parser;
+            parser.InvalidTokenFound += InvalidTokenFound;
+            InitMainParsing();
+        }
+
+        /// <summary>
+        /// When category parsing is completed
+        /// </summary>
+        public EventHandler<Category> CategoryDiscovered { get; set; }
+
+        /// <summary>
+        /// Inits main parsing state. Skips inlines comments, white spaces and new lines and init new cateory parsing when category opening token found.
+        /// </summary>
+        private void InitMainParsing()
+        {
+            parser.ValidTokenFound -= ValidTokenFoundDuringKeyIdParsing;
+            parser.ValidTokenFound += ValidTokenFoundDuringMainParsing;
+
+            parser.AllowedTokens = new HashSet<IToken>
+            {
+                new InlineCommentToken(),
+                new CategoryOpeningToken(),
+            };
+
+            parser.IgnoredTokens = new HashSet<IToken>
+            {
+                new WhiteSpaceToken(),
+                new NewLineToken()
+            };
+        }
+
+        /// <summary>
+        /// Parse only letter tokens, end parsing when closing token found.
+        /// </summary>
+        private void InitCategoryParsing()
+        {
+            parser.ValidTokenFound -= ValidTokenFoundDuringMainParsing;
+            parser.ValidTokenFound -= ValidTokenFoundDuringKeyIdParsing;
+            parser.ValidTokenFound += ValidTokenFoundDuringCategoryParsing;
+            parser.IgnoredTokens = new HashSet<IToken>();
+            parser.AllowedTokens = new HashSet<IToken>
+            {
+                new CategoryClosingToken(),
+                new LetterToken()
+            };
+        }
+
+        /// <summary>
+        /// Parse key id.
+        /// </summary>
+        private void InitKeyIdParsing()
+        {
+            parser.ValidTokenFound -= ValidTokenFoundDuringCategoryParsing;
+            parser.ValidTokenFound += ValidTokenFoundDuringKeyIdParsing;
+
+            parser.AllowedTokens = new HashSet<IToken>
+            {
+                new InlineCommentToken(),
+                new LetterToken(),
+                new EqualityToken(),
+            };
+
+            parser.IgnoredTokens = new HashSet<IToken>();
+        }
+
+        //Parsing top layer
+        private void ValidTokenFoundDuringMainParsing(object sender, IToken token)
+        {
+            switch (token.Type)
+            {
+                case TokenType.InlineComment:
+                    // TODO go to next line
+                    break;
+                case TokenType.CategoryOpening:
+                    currentCategory = new Category();
+                    InitCategoryParsing();
+                    break;
+                default:
+                    throw new InvalidTokenException("Invalid token found during parsing of the file: " + token.Symbol);
+            }
+        }
+
+        //when parsing a category
+        private void ValidTokenFoundDuringCategoryParsing(object sender, IToken token)
+        {
+            switch (token.Type)
+            {
+                case TokenType.CategoryClosing:
+                    InitKeyIdParsing();
+                    break;
+                case TokenType.Letter:
+                    currentCategory.Name += token.Symbol;
+                    break;
+                default:
+                    throw new InvalidTokenException("Invalid token found during parsing of the file: " + token.Symbol);
+            }
+        }
+
+        //when parsing a token id
+        private void ValidTokenFoundDuringKeyIdParsing(object sender, IToken token)
+        {
+            switch (token.Type)
+            {
+                case TokenType.EQ:
+                    break;
+                case TokenType.WhiteSpace:
+                    break;
+                case TokenType.Letter:
+                    if (currentKey == null)
+                    {
+                        currentKey = new Key();
+                    }
+                    currentKey.Id += token.Symbol;
+                    break;
+                case TokenType.LineConcatenator:
+                    break;
+                case TokenType.CategoryOpening:
+                    InitCategoryParsing();
+                    CategoryParsingComplete();
+                    break;
+                case TokenType.InlineComment:
+                    if (currentKey != null && currentKey.KeyValues.Any())
+                    {
+                        throw new InvalidTokenException("Inline comment detected, but key value expected.");
+                    }
+                    else
+                    {
+                        //TODO go to next line
+                    }
+                    break;
+                default:
+                    throw new InvalidTokenException("Invalid token found during parsing of the file: " + token.Symbol);
+            }
+        }
+
+        private void InvalidTokenFound(object sender, IToken token)
+        {
+            throw new InvalidTokenException("Invalid token found during parsing of the file: " + token.Symbol);
+        }
+
+        private void KeyParsingComplete()
+        {
+            currentCategory.Keys.Add(currentKey);
+            currentKey = null;
+        }
+
+        private void CategoryParsingComplete()
+        {
+            CategoryDiscovered?.Invoke(this, currentCategory);
+            currentCategory = null;
+        }
+    }
+}
