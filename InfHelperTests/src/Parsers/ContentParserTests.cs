f58e4203d56e7564576eb735e980ad1d45475068
Jan Skála <j.skala@hotmail.com>
2017-08-13 14:10:21 +0200

    Make the test more dynamic.

--- InfHelperTests/src/Parsers/ContentParserTests.cs
+++ InfHelperTests/src/Parsers/ContentParserTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using InfHelper.Models;
 using InfHelper.Parsers;
@@ -25,17 +26,28 @@ namespace InfHelperTests.Parsers
         [TestMethod()]
         public void MultipleCategoryParsing()
         {
-            string test = "[CATEGORY] \n [CATEGORY2] \n [CATEGORY3]";
+            int CATEGORIES_COUNT = new Random().Next(1,10);
+            var testCategories = new List<string>();
+
+            for (int i = 1; i <= CATEGORIES_COUNT; i++)
+            {
+                testCategories.Add($"[CATEGORY{i}]");
+            }
+
+            string test = string.Join(" \n ", testCategories);
+            
             var categories = new List<Category>();
             var parser = new ContentParser();
             parser.CategoryDiscovered += (sender, category) => categories.Add(category);
 
             parser.Parse(test);
 
-            Assert.IsTrue(categories.Count == 3);
-            Assert.IsTrue(categories.Any(x => x.Name == "CATEGORY"));
-            Assert.IsTrue(categories.Any(x => x.Name == "CATEGORY2"));
-            Assert.IsTrue(categories.Any(x => x.Name == "CATEGORY3"));
+            Assert.IsTrue(categories.Count == CATEGORIES_COUNT);
+
+            for (var i = 1; i <= CATEGORIES_COUNT; i++)
+            {
+                Assert.IsTrue(categories.Any(x => x.Name == $"CATEGORY{i}"));
+            }
         }
     }
 }
