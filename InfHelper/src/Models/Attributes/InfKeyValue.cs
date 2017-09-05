6e6411db3d85ac59bb109a7e629511d6ab8c38f9
Jan Skála <j.skala@hotmail.com>
2017-09-05 10:20:42 +0200

    Add attribute for serialization.

---
+++ InfHelper/src/Models/Attributes/InfKeyValue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InfHelper.Models.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+    public class InfKeyValue : Attribute
+    {
+        private readonly string categoryId;
+        private readonly string keyId;
+
+        public InfKeyValue(string CategoryId, string KeyId)
+        {
+            categoryId = CategoryId;
+            keyId = KeyId;
+        }
+    }
+}
